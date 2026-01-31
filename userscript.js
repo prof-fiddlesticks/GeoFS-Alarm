99450e1914cc2a8b7fb28d4b752bfcfccd938167
prof-fiddlesticks <ishaandokania1@gmail.com>
2026-01-31 11:53:41 -0800

    Add initial userscript for GeoFS alarms

---
+++ userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         GeoFS-Alarm
+// @namespace    https://github.com/prof-fiddlesticks/GeoFS-Alarm
+// @version      0.0.1
+// @description  Adds alarms to GeoFS to keep you updated.
+// @author       prof-fiddlesticks
+// @match        https://www.geo-fs.com/geofs.php*
+// @grant        GM.getResourceUrl
+// @resource     stall   https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/stall_warning.ogg
+// @resource     terrain https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/terrain.ogg
+// @resource     bank    https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/bankangle.ogg
+// ==/UserScript==
