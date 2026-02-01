f7a635e17fc5936852c3dbcbb75e08df115d863c
prof-fiddlesticks <ishaandokania1@gmail.com>
2026-02-01 13:19:29 -0800

    Rename userscript to version 0.0.2

--- userscript_v_0.0.2.js
+++ userscript_v_0.0.2.js
@@ -7,22 +7,37 @@
 // @match        https://www.geo-fs.com/geofs.php*
 // @grant        GM.getResourceUrl
 // @resource     stall   https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/stall_warning.ogg
+// @resource     bank    https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/bank.ogg
 // ==/UserScript==
 
 (function () {
   const G = typeof unsafeWindow !== "undefined" ? unsafeWindow.geofs : geofs;
 
   let wasStalling = false;
+  let wasBanking = false;
+
   let stallSound;
+  let bankSound;
+
+  let lastBankCallout = 0;
+  const cooldownMs = 2500;
+
 
   // Load sound (TM) or fallback (console)
   if (typeof GM !== "undefined") {
     GM.getResourceUrl("stall").then(url => {
       stallSound = new Audio(url);
     });
+    GM.getResourceUrl("bank").then(url => {
+      bankSound = new Audio(url)
+    });
   } else {
     stallSound = new Audio(
-      "https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/stall_warning.ogg"
+      "https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/stall_warning.ogg",
+    bankSound = new Audio(
+      "https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/bankangle.ogg"
+    )
+    
     );
   }
 
@@ -33,8 +48,11 @@
     }
 
     console.log("GeoFS Alarm armed.");
+    
 
     setInterval(() => {
+      const onGround = !!G.animation.values.groundContact;
+
       const isStalling =
         !!G.aircraft.instance.stalling &&
         !G.animation.values.groundContact;
@@ -43,8 +61,23 @@
         stallSound.currentTime = 0;
         stallSound.play();
       }
-
       wasStalling = isStalling;
+
+      const bankDeg = G.animation.values.aroll;
+      const isBanking = !onGround && Math.abs(bankDeg) > 40;
+
+      if (isBanking && bankSound) {
+        const isPlaying = !bankSound.paused && !bankSound.ended;
+        const now = Date.now()
+
+        if (!isPlaying && (now - lastBankCallout >= cooldownMs)) {
+          lastBankCallout = now
+          bankSound.currentTime = 0;
+          bankSound.play()
+        }
+
+      }
+      wasBanking = isBanking;
     }, 200);
   }
 
