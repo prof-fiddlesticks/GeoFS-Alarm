20e35ede4077341164edefbb588188742a6d74c3
prof-fiddlesticks <ishaandokania1@gmail.com>
2026-02-01 20:15:54 -0800

    Update version and add terrain sound functionality

--- userscript_v_0.0.2.js
+++ userscript_v_0.0.2.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         GeoFS-Alarm
 // @namespace    https://github.com/prof-fiddlesticks/GeoFS-Alarm
-// @version      0.0.1
+// @version      0.0.3
 // @description  Adds alarms to GeoFS to keep you updated.
 // @author       prof-fiddlesticks
 // @match        https://www.geo-fs.com/geofs.php*
 // @grant        GM.getResourceUrl
 // @resource     stall   https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/stall_warning.ogg
 // @resource     bank    https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/bankangle.ogg
+// @resource     terrain https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/terrain.ogg
 // ==/UserScript==
 
 (function () {
@@ -18,9 +19,13 @@
 
   let stallSound;
   let bankSound;
+  let terrainSound;
 
   let lastBankCallout = 0;
-  const cooldownMs = 2500;
+  const cooldownmsBank = 2500;
+  let lastTerrainCallout = 0;
+  const cooldownmsTerrain = 4000;
+
 
 
   // Load sound (TM) or fallback (console)
@@ -31,52 +36,79 @@
     GM.getResourceUrl("bank").then(url => {
       bankSound = new Audio(url)
     });
+    GM.getResourceUrl("terrain").then(url => {
+      terrainSound = new Audio(url)
+    });
   } else {
     stallSound = new Audio(
-      "https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/stall_warning.ogg",
+      "https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/stall_warning.ogg"
+    ),
     bankSound = new Audio(
       "https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/bankangle.ogg"
-    )
-    
-    );
+    ),
+    terrainSound = new Audio(
+      "https://raw.githubusercontent.com/prof-fiddlesticks/GeoFS-Alarm/main/terrain.ogg"
+    )    
   }
 
   function waitForGeoFS() {
     if (!G || !G.aircraft || !G.aircraft.instance) {
-      setTimeout(waitForGeoFS, 500);
-      return;
+        setTimeout(waitForGeoFS, 500);
+        return;
     }
 
     console.log("GeoFS Alarm armed.");
     
 
     setInterval(() => {
-      const onGround = !!G.animation.values.groundContact;
-
-      const isStalling =
-        !!G.aircraft.instance.stalling &&
-        !G.animation.values.groundContact;
-
-      if (isStalling && !wasStalling && stallSound) {
-        stallSound.currentTime = 0;
-        stallSound.play();
-      }
-      wasStalling = isStalling;
-
-      const bankDeg = G.animation.values.aroll;
-      const isBanking = !onGround && Math.abs(bankDeg) > 40;
-
-      if (isBanking && bankSound) {
-        const isPlaying = !bankSound.paused && !bankSound.ended;
+        const onGround = !!G.animation.values.groundContact;
         const now = Date.now()
 
-        if (!isPlaying && (now - lastBankCallout >= cooldownMs)) {
-          lastBankCallout = now
-          bankSound.currentTime = 0;
-          bankSound.play()
-        }
+        const isStalling =
+            !!G.aircraft.instance.stalling &&
+            !G.animation.values.groundContact;
 
+        if (isStalling && !wasStalling && stallSound) {
+            stallSound.currentTime = 0;
+            stallSound.play();
       }
+        wasStalling = isStalling;
+
+        const bankDeg = G.animation.values.aroll;
+        const isBanking = !onGround && Math.abs(bankDeg) > 40;
+
+        if (isBanking && bankSound) {
+            const isPlaying = !bankSound.paused && !bankSound.ended;
+
+            if (!isPlaying && (now - lastBankCallout >= cooldownmsBank)) {
+                lastBankCallout = now
+                bankSound.currentTime = 0;
+                bankSound.play()
+            }}
+        function isDescending() {
+            return G.animation.values.verticalSpeed < -400;
+        }
+        function isGearUp() {
+            return G.animation.values.gearPosition == 1;
+        }
+        function seaAltitude() {
+            return G.animation.values.altitude;
+        }
+        function groundAltitude() {
+            return seaAltitude() - G.animation.values.groundElevationFeet - 50;
+        }
+        const steepDescent = G.animation.values.verticalSpeed < -1000
+        if (isDescending() && isGearUp() && groundAltitude() <= 1500 && !onGround && terrainSound && now - lastTerrainCallout >= cooldownmsTerrain) {
+            lastTerrainCallout = now;
+            terrainSound.currentTime = 0;
+            terrainSound.play()
+        }
+        else if (steepDescent && groundAltitude() <= 1500 && !onGround && terrainSound && now - lastTerrainCallout >= cooldownmsTerrain) {
+            lastTerrainCallout = now;
+            terrainSound.currentTime = 0;
+            terrainSound.play()
+        }
+          
       wasBanking = isBanking;
     }, 200);
   }
